adf27b7a8832b25adede63af6d961e7f03a00f9c
Micah Nicodemus <micahnicodemus22@gmail.com>
2016-07-15 14:25:48 -0400

    remove stages branch from code

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714155743) do
+ActiveRecord::Schema.define(version: 20160715182255) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
