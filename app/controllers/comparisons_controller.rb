00c6da7635886e39982391cd30da75467d83f8d0
salesprojames <james@ccsalespro.com>
2016-07-25 13:05:18 -0400

    changes to programs controller

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -1,8 +1,15 @@
 class ComparisonsController < ApplicationController
-	before_action :load_prospect, :load_statement, :load_programs
+	before_action :load_prospect, :load_statement
 	before_action :authenticate_user!
   	before_action :require_subscribed
-  def index  			
+  def index 
+  	@programusers = Programuser.all.where(user_id: current_user.id)
+  	@programs = []
+    @programusers.each do |programuser|
+      @program = Program.find_by_id(programuser.program_id)
+      @programs << @program
+    end	
+    @programs
   end
   def show
   	@program = Program.find(params[:program_id])
@@ -14,8 +21,5 @@ private
 	end
 	def load_statement
 		@statement = @prospect.statements.find(params[:statement_id])
-	end
-	def load_programs
-		@programs = Program.all
 	end
 end
