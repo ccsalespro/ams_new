f39e668b4ec16b75bd29e7a3cca3edd414a8a95b
Micah Nicodemus <micahnicodemus22@gmail.com>
2016-08-17 13:53:26 -0400

    fix interface of added user stuff

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :prospect
-	default_scope -> { order(created_at: :desc) }
+	default_scope -> { order(completed_at: :desc) }
 
 	def completed?
 		!completed_at.blank?
