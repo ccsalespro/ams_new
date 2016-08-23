4b19867361ab256c2dd4ba34845d27de4f16bc33
salesprojames <james@ccsalespro.com>
2016-08-23 16:22:53 -0400

    dependent destroy intcalcitems

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -2,7 +2,7 @@ class Statement < ActiveRecord::Base
   belongs_to :prospect
   has_many :inttableitems, dependent: :destroy
   has_many :inttypes, through: :inttableitems
-  has_many :intcalcitems
+  has_many :intcalcitems, dependent: :destroy
   has_many :comparisons, dependent: :destroy
   validates_presence_of :total_vol
   default_scope -> { order(created_at: :desc) }
