b14eb678c552dad7c19ea1393536a6533317f0a8
micahnico <micahnicodemus22@gmail.com>
2016-09-24 10:56:45 -0400

    fix up teams ui

--- db/schema.rb
+++ db/schema.rb
@@ -507,6 +507,13 @@ ActiveRecord::Schema.define(version: 20160921195046) do
   add_index "prospects", ["user_id", "created_at"], name: "index_prospects_on_user_id_and_created_at"
   add_index "prospects", ["user_id"], name: "index_prospects_on_user_id"
 
+  create_table "sorts", force: :cascade do |t|
+    t.string   "sort_by"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "stages", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
