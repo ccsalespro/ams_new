f2b0458cd6466d71e536a0fcb89bd29273df66af
Ben Shirey <ben@ccsalespro.com>
2016-11-14 09:26:29 -0500

    commit changes to intcalcitem.rb

--- app/models/intcalcitem.rb
+++ app/models/intcalcitem.rb
@@ -2,7 +2,11 @@ class Intcalcitem < ActiveRecord::Base
   belongs_to :inttype
   belongs_to :statement
   belongs_to :prospect
-  
+  def self.import(file)
+		CSV.foreach(file.path, headers: true, :encoding => 'windows-1251:utf-8') do |row|
+			Intcalcitem.create! row.to_hash
+		end	
+	end
   def self.to_csv
 		CSV.generate do |csv|
 			csv << column_names
