16956e71083ff505509fb6ee38456fc55dcb8f11
salesprojames <james@ccsalespro.com>
2017-01-19 11:38:29 -0500

    dependent destroy cc_fields to comparisons

--- app/models/comparison.rb
+++ app/models/comparison.rb
@@ -1,6 +1,6 @@
 class Comparison < ActiveRecord::Base
   belongs_to :statement
   belongs_to :program
-  has_many :cc_fields
+  has_many :cc_fields, dependent: :destroy
   accepts_nested_attributes_for :cc_fields 
 end
