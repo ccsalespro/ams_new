6b22eae8ea760924c12c803aa0ef3ab2644edfaf
micahnico <micahnicodemus22@gmail.com>
2018-06-26 15:18:26 -0400

    add after_create on programs and processors

--- app/models/processor.rb
+++ app/models/processor.rb
@@ -2,4 +2,12 @@ class Processor < ActiveRecord::Base
 	has_many :programs, dependent: :destroy
 	has_many :processorusers, dependent: :destroy
 	has_many :users, through: :processorusers
+
+  after_create :add_users
+
+  def add_users
+    User.all.each do |user|
+      Processoruser.create! user: user, processor: self
+    end
+  end
 end
--- app/models/program.rb
+++ app/models/program.rb
@@ -7,6 +7,14 @@ class Program < ActiveRecord::Base
   has_many :custom_fields
   belongs_to :system
 
+  after_create :add_users
+
+  def add_users
+    User.all.each do |user|
+      Programuser.create! user: user, program: self
+    end
+  end
+
 
   def self.import(file)
 		CSV.foreach(file.path, headers: true, :encoding => 'windows-1251:utf-8') do |row|
